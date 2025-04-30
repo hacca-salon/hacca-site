882eaa7cf9cdbf64d969e57ab18ea0e31d1b60e4
hacca-salon <haccasalon88@gmail.com>
2025-04-30 20:49:57 +0900

    マップ修正
    マップ画像変更

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -62,7 +62,7 @@ export const Contact: React.FC = () => {
           >
             <div className="h-[400px] rounded-lg overflow-hidden shadow-lg">
               <img
-                src="/map3.png"
+                src="/map4.jpg"
                 alt="Map location"
                 className="w-full h-full object-cover"
               />
